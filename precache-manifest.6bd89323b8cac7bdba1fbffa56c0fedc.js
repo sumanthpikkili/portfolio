0ea2ee3185d56db0a374a487ab6586b73b0feb11
Sumanth Pikkili <sumanthpikkili@gmail.com>
2020-01-19 16:14:04 +0000

    Updates

---
+++ precache-manifest.6bd89323b8cac7bdba1fbffa56c0fedc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dfe8c5fa6225bfbc0fbe54c3d714597",
+    "url": "/spikkiliportfolio/index.html"
+  },
+  {
+    "revision": "e02eea6d4a5b68d7a696",
+    "url": "/spikkiliportfolio/static/css/main.4f5d8cdb.chunk.css"
+  },
+  {
+    "revision": "15299915b9056fcc72ec",
+    "url": "/spikkiliportfolio/static/js/2.f6e6191b.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/spikkiliportfolio/static/js/2.f6e6191b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e02eea6d4a5b68d7a696",
+    "url": "/spikkiliportfolio/static/js/main.97589b76.chunk.js"
+  },
+  {
+    "revision": "cb8a767ee84bb2044e02",
+    "url": "/spikkiliportfolio/static/js/runtime-main.79e7624d.js"
+  },
+  {
+    "revision": "6fdf62cc8fa25e6cfb50887fd5c6e1ee",
+    "url": "/spikkiliportfolio/static/media/FacebookIcon.6fdf62cc.svg"
+  },
+  {
+    "revision": "c257142416811a292ef7f268e9435478",
+    "url": "/spikkiliportfolio/static/media/InstaIcon.c2571424.svg"
+  },
+  {
+    "revision": "406464ed11bd2c484a99ecc9870acb4a",
+    "url": "/spikkiliportfolio/static/media/LinkedInIcon.406464ed.svg"
+  }
+]);
